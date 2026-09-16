89c66acf3b916fdc64114122cc274fd1b42d671b
Lucas Lopes <lucas.lpsbn@gmail.com>
2026-09-16 19:54:12 -0300

    adicionando nova regra de negocio

--- funcoes.js
+++ funcoes.js
@@ -2,12 +2,16 @@ function calcularMedia(nota1, nota2) {
  return (nota1 + nota2) / 2;
 }
 
-function verificarSituacao(media) {
-    if (media >= 6) {
-        return "Aprovado";
-    }
+function verificarSituacao (media) {
+   if (media >= 6) {
+       return "Aprovado";
+   }
 
-    return "Reprovado";
+   if (media >= 4) {
+	return "Recuperação";
+   }
+
+   return "Reprovado";
 }
 
 if (typeof module !== "undefined") {
